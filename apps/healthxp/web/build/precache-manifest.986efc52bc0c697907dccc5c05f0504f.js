a1e22b84ec9e9b15bad32d343d91db1ae46a4c81
Andreza <andrezasilva744@gmail.com>
2023-07-19 21:40:38 -0300

    Sobre o projeto em si

---
+++ apps/healthxp/web/build/precache-manifest.986efc52bc0c697907dccc5c05f0504f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f793abe3e64504ad6e9aaa0c514c99a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4372924e1c5e07ef169d",
+    "url": "/static/css/2.18fb2ca0.chunk.css"
+  },
+  {
+    "revision": "4372924e1c5e07ef169d",
+    "url": "/static/js/2.27b8811c.chunk.js"
+  },
+  {
+    "revision": "2a951bc2a2cbd1448db6",
+    "url": "/static/js/main.4eaad246.chunk.js"
+  },
+  {
+    "revision": "d57174236681d2be10b3",
+    "url": "/static/js/runtime-main.422271b2.js"
+  },
+  {
+    "revision": "d4b4377fdaa3742f521ce4109df5f180",
+    "url": "/static/media/background.d4b4377f.jpg"
+  },
+  {
+    "revision": "834aba9c3085ae6ff786c8b14e60e639",
+    "url": "/static/media/logo.834aba9c.svg"
+  }
+]);

8f8cf1a981615520d5e5fef79461eb9eca921e57
Andreza <dedezasilva@live.com>
2023-07-23 17:18:35 -0300

    feat: Cy.fixture

--- projects/healthxp-dual/web/cypress/e2e/login.cy.js
+++ projects/healthxp-dual/web/cypress/e2e/login.cy.js
@@ -1,22 +1,11 @@
 describe ('login', () => {
 	it('deve logar com o perfil do admin', () => {
 		//Dado que eu tenho um usuário admin cadastrado
-	
-		// Quando faço login no gestor de academias
-		cy.visit('http://localhost:3000')
-		cy.get('input[name=email]').type(user.email)
-		cy.get('input[name=password]').type(user.password)
-		
-		//cy.contains('button', 'Entrar')
-		cy.contains('aside .logged-user', 'Olá, ' + user.name)
-			.click()
-		
-		// Então devo ver o dashboard
-		//cy.get('.logged-user')
-		//	.should('be.visible')
-		//	.should('have.text', 'Olá, Admin ') -> Aqui ele compara um texto com outro 
-		cy.contains('aside .logged-user', 'Olá, Admin') //-> Aqui ele ignora o espacinho, usa o texto alvo para combinar com a busca e verificar se está visível 
-			.should('be.visible')
+		//
+		cy.fixture('users.json').then(function(users){
+			cy.log(users)
+		})
+		// Se eu rodar sem as outras informações no cypress aparece um Objet{3}
 			
 	})
 })

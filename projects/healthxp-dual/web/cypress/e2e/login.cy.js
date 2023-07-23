7b45060f15036bd366f54fa7ac7f61972f9f18b0
Andreza <dedezasilva@live.com>
2023-07-23 17:53:43 -0300

    feat: criação de cenários novos

--- projects/healthxp-dual/web/cypress/e2e/login.cy.js
+++ projects/healthxp-dual/web/cypress/e2e/login.cy.js
@@ -1,22 +1,61 @@
+import users from '../fixtures/users.json'
 describe ('login', () => {
-	it('deve logar com o perfil do admin', () => {
-		//Dado que eu tenho um usuário admin cadastrado
-	
-		// Quando faço login no gestor de academias
+	it('deve logar com o perfil do admin',  () => {
+		const user = users.admin
+		cy.log(JSON.stringify(user))
 		cy.visit('http://localhost:3000')
 		cy.get('input[name=email]').type(user.email)
 		cy.get('input[name=password]').type(user.password)
-		
-		//cy.contains('button', 'Entrar')
+		cy.contains('button', 'Entrar').click()
+
 		cy.contains('aside .logged-user', 'Olá, ' + user.name)
 			.click()
 		
-		// Então devo ver o dashboard
-		//cy.get('.logged-user')
-		//	.should('be.visible')
-		//	.should('have.text', 'Olá, Admin ') -> Aqui ele compara um texto com outro 
-		cy.contains('aside .logged-user', 'Olá, Admin') //-> Aqui ele ignora o espacinho, usa o texto alvo para combinar com a busca e verificar se está visível 
+
+		cy.contains('aside .logged-user', 'Olá, Admin')
 			.should('be.visible')
 			
+	})
+
+	it('não deve logar com senha incorreta', () => {
+		const user = users.inv_pass
+
+		cy.visit('http://localhost:3000')
+		cy.get('input[name=email]').type(user.email)
+		cy.get('input[name=password]').type(user.password)
+		cy.contains('button', 'Entrar').click()
+
+		cy.get('#swal2-content')
+			.should('be.visible')
+			.should('have.text', 'Suas credenciais são inválidas, por favor tente novamente!')
+
+	})
+
+	it('não deve logar com email não cadastrado', () => {
+		const user = users.email_not_found
+
+		cy.visit('http://localhost:3000')
+		cy.get('input[name=email]').type(user.email)
+		cy.get('input[name=password]').type(user.password)
+		cy.contains('button', 'Entrar').click()
+
+		cy.get('#swal2-content')
+			.should('be.visible')
+			.should('have.text', 'Suas credenciais são inválidas, por favor tente novamente!')
+
+	})
+
+	it('não deve logar com email incorreto', () => {
+		const user = users.inv_email
+
+		cy.visit('http://localhost:3000')
+		cy.get('input[name=email]').type(user.email)
+		cy.get('input[name=password]').type(user.password)
+		cy.contains('button', 'Entrar').click()
+
+		cy.get('#swal2-content')
+			.should('be.visible')
+			.should('have.text', 'Insira um email válido.')
+
 	})
 })

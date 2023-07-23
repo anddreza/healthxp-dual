94b8daf6bf63a84303fc937c47568993de33a48e
Andreza <dedezasilva@live.com>
2023-07-23 17:28:06 -0300

    feat: Implementado fixture e o before

--- projects/healthxp-dual/web/cypress/e2e/login.cy.js
+++ projects/healthxp-dual/web/cypress/e2e/login.cy.js
@@ -1,11 +1,23 @@
 describe ('login', () => {
+
+	before(() => {
+		cy.fixture('users.json').then((user)=> {
+			this.user =user
+		})
+	})
 	it('deve logar com o perfil do admin', () => {
 		//Dado que eu tenho um usuário admin cadastrado
 		//
-		cy.fixture('users.json').then(function(users){
-			cy.log(users)
+		cy.fixture('users.json').then(function(user){
+			cy.log(JSON.stringify(user))
+			// só para conseguir verifica o log
+			cy.visit('http://localhost:3000')
+			cy.get('input[name=email]').type(user.email)
+			cy.get('input[name=password]').type(user.password)
+			cy.contains('button', 'Entrar').click()
+	
+			cy.contains('aside .logged-user', 'Olá, ' + user.name)
+				.should('be.visible')
 		})
-		// Se eu rodar sem as outras informações no cypress aparece um Objet{3}
-			
 	})
 })

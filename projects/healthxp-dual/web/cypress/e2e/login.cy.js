c15d5d2a834d4d9fed9c4caffa5453d632c85baf
Andreza <dedezasilva@live.com>
2023-07-23 12:41:04 -0300

    feat: encontrar elementos da página de login

---
+++ projects/healthxp-dual/web/cypress/e2e/login.cy.js
@@ -0,0 +1,23 @@
+describe ('login', () => {
+	it('deve logar com o perfil do admin', () => {
+		//Dado que eu tenho um usuário admin cadastrado
+		const user = {
+			email: 'admin@healthxp.com',
+			password: 'xperience'
+		}
+		// Quando faço login no gestor de academias
+		cy.visit('http://localhost:3000')
+		cy.get('#email').type(user.email)
+		cy.get('#password').type(user.password)
+		cy.contains('button', 'Entrar')
+			.click()
+		
+		// Então devo ver o dashboard
+		//cy.get('.logged-user')
+		//	.should('be.visible')
+		//	.should('have.text', 'Olá, Admin ') -> Aqui ele compara um texto com outro 
+		cy.contains('aside .logged-user', 'Olá, Admin') //-> Aqui ele ignora o espacinho, usa o texto alvo para combinar com a busca e verificar se está visível 
+			.should('be.visible')
+			
+	})
+})

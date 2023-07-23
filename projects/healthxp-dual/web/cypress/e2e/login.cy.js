08bbe1438d5b6ade935d8a1da73e249d4c7e4371
Andreza <dedezasilva@live.com>
2023-07-23 17:32:22 -0300

    feat: explicação sobre a função convencional e a arrow function

--- projects/healthxp-dual/web/cypress/e2e/login.cy.js
+++ projects/healthxp-dual/web/cypress/e2e/login.cy.js
@@ -1,14 +1,17 @@
+import user from '../fixtures/users.json'
 describe ('login', () => {
 
-	before(() => {
+	// Antes disso ele falhava, pois estava com arroy function, a alteração para função convencional implica em que o cypress consegue obter a massa de teste antes do teste começar, porém só funciona com função convencional 
+	before(function () {
 		cy.fixture('users.json').then((user)=> {
+			// O JS só consegue ter acesso a variável de contexto que é o this se a função for convencional, se for função de seta ela não funciona
 			this.user =user
 		})
 	})
-	it('deve logar com o perfil do admin', () => {
+	it('deve logar com o perfil do admin', function () {
 		//Dado que eu tenho um usuário admin cadastrado
 		//
-		cy.fixture('users.json').then(function(user){
+		const user = this.user
 			cy.log(JSON.stringify(user))
 			// só para conseguir verifica o log
 			cy.visit('http://localhost:3000')
@@ -18,6 +21,6 @@ describe ('login', () => {
 	
 			cy.contains('aside .logged-user', 'Olá, ' + user.name)
 				.should('be.visible')
-		})
+		
 	})
 })

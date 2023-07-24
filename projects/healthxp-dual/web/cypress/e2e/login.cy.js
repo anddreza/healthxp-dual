cf389d9f666a0fa3163d79e7c0c82cae21e7d3a0
Andreza <dedezasilva@live.com>
2023-07-24 09:40:42 -0300

    feat: alteração dos campos, fazendo reuso

--- projects/healthxp-dual/web/cypress/e2e/login.cy.js
+++ projects/healthxp-dual/web/cypress/e2e/login.cy.js
@@ -2,11 +2,12 @@ import users from '../fixtures/users.json'
 describe ('login', () => {
 	it('deve logar com o perfil do admin',  () => {
 		const user = users.admin
-		cy.log(JSON.stringify(user))
-		cy.visit('http://localhost:3000')
-		cy.get('input[name=email]').type(user.email)
-		cy.get('input[name=password]').type(user.password)
-		cy.contains('button', 'Logar').click()
+		cy.doLogin(user)
+		//Isso foi centralizado lá no Cypress.Commands.add
+		//cy.visit('http://localhost:3000')
+		//cy.get('input[name=email]').type(user.email)
+		//cy.get('input[name=password]').type(user.password)
+		//cy.contains('button', 'Logar').click()
 
 		cy.contains('aside .logged-user', 'Olá, ' + user.name)
 		   .should('be.visible')		
@@ -15,76 +16,39 @@ describe ('login', () => {
 	it('não deve logar com senha incorreta', () => {
 		const user = users.inv_pass
 
-		cy.visit('http://localhost:3000')
-		cy.get('input[name=email]').type(user.email)
-		cy.get('input[name=password]').type(user.password)
-		cy.contains('button', 'Entrar').click()
-
-		cy.get('#swal2-content')
-			.should('be.visible')
-			.should('have.text', 'Suas credenciais são inválidas, por favor tente novamente!')
+		cy.doLogin(user)
+		cy.popUpHave('Suas credenciais são inválidas, por favor tente novamente!')
 
 	})
 
 	it('não deve logar com email não cadastrado', () => {
 		const user = users.email_not_found
 
-		cy.visit('http://localhost:3000')
-		cy.get('input[name=email]').type(user.email)
-		cy.get('input[name=password]').type(user.password)
-		cy.contains('button', 'Entrar').click()
-
-		cy.get('#swal2-content')
-			.should('be.visible')
-			.should('have.text', 'Suas credenciais são inválidas, por favor tente novamente!')
+		cy.doLogin(user)
+		cy.popUpHave('Suas credenciais são inválidas, por favor tente novamente!')
 
 	})
 
 	it('não deve logar com email incorreto', () => {
 		const user = users.inv_email
 
-		cy.visit('http://localhost:3000')
-		cy.get('input[name=email]').type(user.email)
-		cy.get('input[name=password]').type(user.password)
-		cy.contains('button', 'Entrar').click()
-
-		cy.get('#swal2-content')
-			.should('be.visible')
-			.should('have.text', 'Insira um email válido.')
+		cy.doLogin(user)
+		cy.popUpHave('Insira um email válido.')
 
 	})
 
 	it('não deve logar com email em branco', () => {
 		const user = users.empty_email
 
-		cy.visit('http://localhost:3000')
-		if(user.email){
-			cy.get('input[name=email]').type(user.email)
-		}
-		cy.get('input[name=password]').type(user.password)	
-		cy.contains('button', 'Entrar').click()
-
-		cy.get('#swal2-content')
-			.should('be.visible')
-			.should('have.text', 'Os campos email e senha são obrigatórios.')
+		cy.doLogin(user)
+		cy.popUpHave('Os campos email e senha são obrigatórios.')
 
 	})
 
 	it('não deve logar com senha em branco', () => {
 		const user = users.empty_password
-
-		cy.visit('http://localhost:3000')
-		if(user.email){
-			cy.get('input[name=email]').type(user.email)
-		}
-		if(user.password){
-			cy.get('input[name=password]').type(user.password)		
-		}
-		cy.contains('button', 'Entrar').click()
-
-		cy.get('#swal2-content')
-			.should('be.visible')
-			.should('have.text', 'Os campos email e senha são obrigatórios.')
+		cy.doLogin(user)
 
 	})
 })
+

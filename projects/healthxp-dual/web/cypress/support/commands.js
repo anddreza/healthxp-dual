240508f5a63d5455040bbeb43fdbf72cd91879c1
Andreza <dedezasilva@live.com>
2023-07-24 09:41:25 -0300

    feat: realizando reuso na página de login

--- projects/healthxp-dual/web/cypress/support/commands.js
+++ projects/healthxp-dual/web/cypress/support/commands.js
@@ -23,3 +23,21 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('doLogin', (user) => {
+	cy.visit('http://localhost:3000')
+
+	if(user.email){
+		cy.get('input[name=email]').type(user.email)
+	}
+
+	if(user.password){
+		cy.get('input[name=password]').type(user.password)
+	}
+		cy.contains('button', 'Entrar').click()
+})
+
+Cypress.Commands.add('popUpHave', (text) => {
+	cy.get('#swal2-content')
+			.should('be.visible')
+			.should('have.text', text)
+})

8d886f2d131f02714026c99150b1bf5ecc18ccec
Andreza <dedezasilva@live.com>
2023-07-25 10:33:48 -0300

    feat: inclusão do cenário de remover um aluno sem matricula

--- projects/healthxp-dual/web/cypress/support/pages/StudentPage.js
+++ projects/healthxp-dual/web/cypress/support/pages/StudentPage.js
@@ -1,8 +1,10 @@
 import navbar from "./components/Navbar"
+import popup from "./components/Popup"
 class StudentPage{
 
 	constructor(){
 		this.navbar = navbar
+		this.popup = popup
 	}
 
 	goToRegister(){
@@ -18,10 +20,19 @@ class StudentPage{
 
 		cy.contains('button', 'Cadastrar').click()
 	}
-	popUpHave(expectedText){
-		cy.get('#swal2-content')
-			.should('be.visible')
-			.should('have.text', expectedText)
+	//popUpHave(expectedText){
+	//	cy.get('#swal2-content')
+	//		.should('be.visible')
+	//		.should('have.text', expectedText)
+	//}
+	search(name){
+		cy.get('input[placeholder="Buscar por nome"]').type(name)
+	}
+
+	remove(email){
+		cy.contains('tr', email, {timeout: 10000})
+			.find('button')
+			.click()
 	}
 }
 

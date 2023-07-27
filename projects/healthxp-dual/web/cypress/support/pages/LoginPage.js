cdfc02034c69d6da2cb90ad57423d5f0afe06eec
Andreza <dedezasilva@live.com>
2023-07-26 21:01:46 -0300

    feat: aprendendo sobre if ternário

--- projects/healthxp-dual/web/cypress/support/pages/LoginPage.js
+++ projects/healthxp-dual/web/cypress/support/pages/LoginPage.js
@@ -10,17 +10,26 @@ class LoginPage{
 		cy.visit('http://localhost:3000')
 	}
 	fill(user){
-		if(user.email){
-			cy.get('input[name=email]')
+		cy.get('input[name=email]').clear({force: true}).as('email')
+		cy.get('input[name=password]').clear({force: true}).as('password')
+
+		user.email ? cy.get('@email').type(user.email) : cy.log('empty email')
+		user.password ? cy.get('@password').type(user.password) : cy.log('empty password')
+
+
+		/*if(user.email){
+			cy.get('@email')
 				.clear({force: true})
 				.type(user.email)
+		} else {
+			cy.log('empty email')
 		}
 	
 		if(user.password){
-			cy.get('input[name=password]')
+			cy.get('@password')
 				.clear({force: true})
 				.type(user.password)
-		}
+		}*/
 	}
 	submit(){	
 		cy.contains('button', 'Entrar').click()

d67c495cdd84da59d509c790ef6cf231c4f07199
Andreza <dedezasilva@live.com>
2023-09-04 12:45:16 -0300

    feat: foi finalizado assim a alteração dessas chamadas da API

--- projects/healthxp-dual/helpers/app.js
+++ projects/healthxp-dual/helpers/app.js
@@ -11,12 +11,14 @@ app.get('/', function (req, res) {
   res.json({ message: 'Hello API Helper'})
 })
 
-app.post('./students', db.deleteAndCreateStudent)
+app.post('/students', db.deleteAndCreateStudent)
+app.delete('/students/:email', db.deleteStudentByEmail)
+app.get('/students/:email', db.selectStudent)
+
+app.listen(5000)	
+
 //app.post('/students', function(req, res){
 	//const student = req.body //corpo da requisição que está sendo enviada
 	//console.log(req.body) //API retornando como undefined, por causa disso foi criada o const app = express()
 	//res.json('./students', db.deleteAndCreateStudent)
 //})
-app.delete('/students/:email', db.deleteStudentByEmail)
-
-app.listen(5000)	
--- projects/healthxp-dual/helpers/db.js
+++ projects/healthxp-dual/helpers/db.js
@@ -9,14 +9,15 @@ const pool = new Pool({
 })
 
 const deleteAndCreateStudent = (req, res) => {
+
 	const student = req.body 
+
 	const query = `
 	WITH add AS(
 	  INSERT INTO students(name, email, age, weight, feet_tall) VALUES ($1, $2, $3, $4, $5)
 	)
 	DELETE FROM students WHERE email = $2;
 	`
-
 	const values = [
 		student.name, student.email, student.age, student.weight, student.feet_tall
 	]
@@ -30,7 +31,7 @@ const deleteAndCreateStudent = (req, res) => {
 
 const deleteStudentByEmail = (req, res) => {
 
-	const email = req.params.email
+	const studentEmail = req.params.email
 
 	const query = 'DELETE FROM students WHERE email = $1;'
 	pool.query(query, [studentEmail], function(error, result){
@@ -42,7 +43,20 @@ const deleteStudentByEmail = (req, res) => {
 
 }
 
+const selectStudent = (req, res) => {
+	const studentEmail = req.params.email
+	
+	const query = 'SELECT id FROM students WHERE email = $1;'
+	pool.query(query, [studentEmail], function(error, result){
+	  if(error){
+		return res.status(500).json(error)
+	  }
+	  res.status(200).json(result.rows[0])
+	})
+}
+
 module.exports = {
 	deleteAndCreateStudent,
-	deleteStudentByEmail
+	deleteStudentByEmail,
+	selectStudent
 }

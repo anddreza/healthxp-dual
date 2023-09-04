bfbe91fbec3ef69d7456a2519894d60cfe5650a8
Andreza <dedezasilva@live.com>
2023-09-04 08:49:07 -0300

    feat: alterando as rotas para não trabalhar com stacks, necessário revisar, primeiros passos da construção da API Helper

---
+++ projects/healthxp-dual/helpers/app.js
@@ -0,0 +1,22 @@
+//o codigo que vai fazer o require no express
+const express = require('express')
+//instancia do express
+const app = express()
+
+app.use(express.json())
+
+const db = require('./db')
+
+app.get('/', function (req, res) {
+  res.json({ message: 'Hello API Helper'})
+})
+
+app.post('./students', db.deleteAndCreateStudent)
+//app.post('/students', function(req, res){
+	//const student = req.body //corpo da requisição que está sendo enviada
+	//console.log(req.body) //API retornando como undefined, por causa disso foi criada o const app = express()
+	//res.json('./students', db.deleteAndCreateStudent)
+//})
+app.delete('/students/:email', db.deleteStudentByEmail)
+
+app.listen(5000)	

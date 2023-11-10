846b8ac0cc58e7bdb6f3b0c55833546607ac2fb0
Andreza <dedezasilva@live.com>
2023-11-10 14:58:10 -0300

    feat: inserção de enrollments

--- projects/healthxp-dual/helpers/db.js
+++ projects/healthxp-dual/helpers/db.js
@@ -15,6 +15,7 @@ const deleteAndCreateStudent = (req, res) => {
 	const query = `
 	WITH add AS(
 	  INSERT INTO students(name, email, age, weight, feet_tall) VALUES ($1, $2, $3, $4, $5)
+	  RETURNING id
 	)
 	DELETE FROM students WHERE email = $2;
 	`
@@ -43,7 +44,34 @@ const deleteStudentByEmail = (req, res) => {
 
 }
 
-const selectStudent = (req, res) => {
+const insertEnrollByEmail = (req, res) => {
+
+	const {email, plan_id, price} = req.body
+
+	const query = `
+	INSERT INTO enrollments(enrollment_code, student_id, plan_id, credit_card, status, price)
+	SELECT 
+		'XPT0123' as enrollment_code,
+		id as student_id, 
+		$2 as plan_id,
+		'4242' as credit_card,
+		true as status,
+		$3 as price
+	FROM students 
+	WHERE email = $1;
+	`
+
+	const values = [email, plan_id, price]
+
+	pool.query(query, values, function(error, result){
+	  if(error){
+		return res.status(500).json(error)
+	  }
+	  res.status(204).end()
+	})	
+}
+
+/* const selectStudent = (req, res) => {
 	const studentEmail = req.params.email
 	
 	const query = 'SELECT id FROM students WHERE email = $1;'
@@ -53,10 +81,10 @@ const selectStudent = (req, res) => {
 	  }
 	  res.status(200).json(result.rows[0])
 	})
-}
+} */
 
 module.exports = {
 	deleteAndCreateStudent,
 	deleteStudentByEmail,
-	selectStudent
+	insertEnrollByEmail
 }

3f12acc6cad5d38d2c53275110ebe853a3ff0e70
Andreza <dedezasilva@live.com>
2023-11-13 10:50:10 -0300

    feat: organização da casa

--- projects/healthxp-dual/web/cypress/support/commands.js
+++ projects/healthxp-dual/web/cypress/support/commands.js
@@ -1,28 +1,3 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 /*Cypress.Commands.add('doLogin', (user) => {
 	// temos uma função que faz login
 	cy.visit('http://localhost:3000')
@@ -42,7 +17,9 @@ Cypress.Commands.add('popUpHave', (text) => {
 			.should('be.visible')
 			.should('have.text', text)
 })*/
-import { data } from 'cypress/types/jquery'
+
+
+//import { data } from 'cypress/types/jquery'
 import users from '../fixtures/users.json'
 import loginPage from './pages/LoginPage'
 import studentPage from './pages/StudentPage'

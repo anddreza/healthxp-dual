c4adeed4586c2952692e11ff36d89bfd0238a656
Andreza <dedezasilva@live.com>
2023-11-14 17:13:55 -0300

    feat: alteração de dados sensíveis, .env, quebra da query para buscar id

--- projects/healthxp-dual/helpers/db.js
+++ projects/healthxp-dual/helpers/db.js
@@ -1,32 +1,42 @@
+require('dotenv').config()
 const { Pool } = require('pg')
 
 const pool = new Pool({
-	host: 'babar.db.elephantsql.com',
-	user: 'sqlgdrtq',
-	password: 'CPA61raaDqbG4pAJuVtrvvXdKJ9w55fG',
-	database: 'sqlgdrtq',
-	port: 5432
+	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASS,
+	database: process.env.DB_NAME,
+	port: process.env.DB_PORT
 })
 
 const deleteAndCreateStudent = (req, res) => {
 
-	const student = req.body 
+	const student = req.body
 
-	const query = `
-	WITH add AS(
-	  INSERT INTO students(name, email, age, weight, feet_tall) VALUES ($1, $2, $3, $4, $5)
-	  RETURNING id
-	)
-	DELETE FROM students WHERE email = $2;
+	const queryDelete = `
+	DELETE FROM students WHERE email = $1;
+	`
+
+	const queryInsert = `
+	  INSERT INTO students(name, email, age, weight, feet_tall) 
+	  VALUES ($1, $2, $3, $4, $5)
+	  RETURNING id as student_id;
 	`
 	const values = [
 		student.name, student.email, student.age, student.weight, student.feet_tall
 	]
-	pool.query(query, values, function (error, result) {
+
+	pool.query(queryDelete, [student.email], function (error, result) {
 		if (error) {
 			return res.status(500).json(error)
 		}
-		res.status(201).json(result)
+
+		pool.query(queryInsert, values, function (error, result) {
+			if (error) {
+				return res.status(500).json(error)
+			}
+			res.status(201).json({ student_id: result.rows[0].student_id })
+		})
 	})
 }
 
@@ -35,18 +45,18 @@ const deleteStudentByEmail = (req, res) => {
 	const studentEmail = req.params.email
 
 	const query = 'DELETE FROM students WHERE email = $1;'
-	pool.query(query, [studentEmail], function(error, result){
-	  if(error){
-		return res.status(500).json(error)
-	  }
-	  res.status(204).end()
+	pool.query(query, [studentEmail], function (error, result) {
+		if (error) {
+			return res.status(500).json(error)
+		}
+		res.status(204).end()
 	})
 
 }
 
 const insertEnrollByEmail = (req, res) => {
 
-	const {email, plan_id, price} = req.body
+	const { email, plan_id, price } = req.body
 
 	const query = `
 	INSERT INTO enrollments(enrollment_code, student_id, plan_id, credit_card, status, price)
@@ -63,12 +73,12 @@ const insertEnrollByEmail = (req, res) => {
 
 	const values = [email, plan_id, price]
 
-	pool.query(query, values, function(error, result){
-	  if(error){
-		return res.status(500).json(error)
-	  }
-	  res.status(204).end()
-	})	
+	pool.query(query, values, function (error, result) {
+		if (error) {
+			return res.status(500).json(error)
+		}
+		res.status(204).end()
+	})
 }
 
 /* const selectStudent = (req, res) => {

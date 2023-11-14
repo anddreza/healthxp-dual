7ddeee57b841df871a2bb58329e51d2c3b44d3a0
Andreza <dedezasilva@live.com>
2023-11-14 17:12:50 -0300

    feat: inserção do validator, modelei para que os campos sempre fossem obrigatórios, app.use para o validation

--- projects/healthxp-dual/helpers/app.js
+++ projects/healthxp-dual/helpers/app.js
@@ -2,6 +2,8 @@
 const express = require('express')
 
 const Joi = require('joi')
+const validator = require('express-joi-validation').createValidator({passError: true})
+
 //instancia do express
 const app = express()
 
@@ -13,6 +15,8 @@ app.get('/', function (req, res) {
   res.json({ message: 'Hello API Helper'})
 })
 
+
+//modelar o cadastro de alunos 
 const studentSchema = Joi.object({
 		name: Joi.string().required(),
 		email: Joi.string().email().required(),
@@ -21,10 +25,23 @@ const studentSchema = Joi.object({
 		feet_tall: Joi.number().required()
 })
 
-app.post('/students', db.deleteAndCreateStudent)
+app.post('/students', validator.body(studentSchema), db.deleteAndCreateStudent)
 app.delete('/students/:email', db.deleteStudentByEmail)
 /* app.get('/students/:email', db.selectStudent) */
 app.post('/enrolls', db.insertEnrollByEmail)
+
+app.use((err, req, res, next) => {
+	if (err && err.error && err.error.isJoi) {
+	  // we had a joi error, let's return a custom 400 json response
+	  res.status(400).json({
+		type: err.type, // will be "query" here, but could be "headers", "body", or "params"
+		message: err.error.toString()
+	  });
+	} else {
+	  // pass on to another error handler
+	  next(err);
+	}
+  });
 
 app.listen(5000)	
 

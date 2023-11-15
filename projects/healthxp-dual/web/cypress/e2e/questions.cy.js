93d5ae506d011ad6b69598183d6aadab95661823
Andreza <dedezasilva@live.com>
2023-11-15 08:24:20 -0300

    feat: inserção de novo cenário

--- projects/healthxp-dual/web/cypress/e2e/questions.cy.js
+++ projects/healthxp-dual/web/cypress/e2e/questions.cy.js
@@ -1,13 +1,34 @@
 import data from '../fixtures/questions.json'
-import enrollsPage from '../support/pages/components/EnrollsPage'
+import studentPage from  '../support/pages/StudentPage'
 
-describe('receber perguntas', ()=> {
-	it('deve poder recebr uma notificação com uma pergunta do aluno', ()=>{
-		
-		const dataTest = data.create;
+describe('receber perguntas', () => {
+	it('deve poder recebr uma notificação com uma pergunta do aluno', () => {
+
+		const dataTest = data.notification
 		cy.resetStudent(dataTest.student)
 		cy.createEnroll(dataTest)
 		cy.createQuestion(dataTest.question)
+
+		cy.adminLogin()
+
+		studentPage.navbar.openNotifications()
+		studentPage.notifications.haveQuestion(dataTest.question)
+
+	})
+
+	it('deve poder responder uma pergunta de alunos', () => {
+		const dataTest = data.prof_answer
+		cy.resetStudent(dataTest.student)
+		cy.createEnroll(dataTest)
+		cy.createQuestion(dataTest.question)
+
+		cy.adminLogin()
+		
+		studentPage.navbar.openNotifications()
+		studentPage.notifications.openQuestion(dataTest.question)
+		studentPage.notifications.sendAnswer(dataTest.answer)
+
+		studentPage.popup.haveText('Resposta enviada com sucesso')
 
 	})
 })
